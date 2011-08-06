3ea1f547edf848259b9fae1adb50d5dddc2576ac
wolfgang.engel@gmail.com <wolfgang.engel@gmail.com@da5beada-464d-0410-a84d-2b73e14d68f6>
2011-08-06 20:14:36 +0000

    forgot to commit the right bat file.

--- trunk/BuildSmall.bat
+++ trunk/BuildSmall.bat
@@ -1,4 +1,4 @@
 SET PATH=C:\Program Files (x86)\Microsoft DirectX SDK (June 2010)\Lib\x86;C:\Program Files\Microsoft SDKs\Windows\v7.1\Lib
-Tools\crinkler12\crinkler.exe /OUT:intro.exe /SUBSYSTEM:WINDOWS /COMPMODE:FAST /ORDERTRIES:1000 /PRINT:IMPORTS /PRINT:LABELS kernel32.lib user32.lib winmm.lib d3d10.lib d3dx10d.lib output\Window.obj
+Tools\crinkler13\crinkler.exe /OUT:intro.exe /SUBSYSTEM:WINDOWS /COMPMODE:FAST /ORDERTRIES:1000 /PRINT:IMPORTS /PRINT:LABELS kernel32.lib user32.lib winmm.lib d3d10.lib d3dx10d.lib output\Window.obj
 copy /B /Y Intro.exe Final\Intro.exe
 
